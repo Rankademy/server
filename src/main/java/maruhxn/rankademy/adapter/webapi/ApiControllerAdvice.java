01e3c091c9a5d1ba1d0ddea692fc3e87e1a6b441
maruhxn <maruhan1016@gmail.com>
2025-07-21 05:03:39 +0900

    refactor: ProblemDetailBuilder

--- src/main/java/maruhxn/rankademy/adapter/webapi/ApiControllerAdvice.java
+++ src/main/java/maruhxn/rankademy/adapter/webapi/ApiControllerAdvice.java
@@ -1,5 +1,6 @@
 package maruhxn.rankademy.adapter.webapi;
 
+import maruhxn.rankademy.adapter.util.ProblemDetailBuilder;
 import maruhxn.rankademy.domain.user.exception.DuplicateUsernameException;
 import maruhxn.rankademy.domain.user.exception.RequiredUnivInfoException;
 import org.springframework.http.HttpStatus;
@@ -8,33 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.time.LocalDateTime;
-
 @RestControllerAdvice
 public class ApiControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(DuplicateUsernameException.class)
     public ProblemDetail duplicateUsernameExceptionHandler(DuplicateUsernameException ex) {
-        return buildProblemDetail(HttpStatus.CONFLICT, ex);
+        return ProblemDetailBuilder.build(HttpStatus.CONFLICT, ex);
     }
 
     @ExceptionHandler(RequiredUnivInfoException.class)
     public ProblemDetail requiredUnivInfoExceptionHandler(RequiredUnivInfoException ex) {
-        return buildProblemDetail(HttpStatus.BAD_REQUEST, ex);
+        return ProblemDetailBuilder.build(HttpStatus.BAD_REQUEST, ex);
     }
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail exceptionHandler(Exception ex) {
 
-        return buildProblemDetail(HttpStatus.INTERNAL_SERVER_ERROR, ex);
-    }
-
-    private static ProblemDetail buildProblemDetail(HttpStatus status, Exception exception) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, exception.getMessage());
-
-        problemDetail.setProperty("timestamp", LocalDateTime.now());
-        problemDetail.setProperty("exception", exception.getClass().getSimpleName());
-
-        return problemDetail;
+        return ProblemDetailBuilder.build(HttpStatus.INTERNAL_SERVER_ERROR, ex);
     }
 }
