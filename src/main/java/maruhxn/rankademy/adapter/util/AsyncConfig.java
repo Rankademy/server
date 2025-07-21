a2870d00bf3362735dd6384101da61f5be101853
maruhxn <maruhan1016@gmail.com>
2025-07-22 02:18:01 +0900

    chore: Add AsyncConfig

---
+++ src/main/java/maruhxn/rankademy/adapter/util/AsyncConfig.java
@@ -0,0 +1,10 @@
+package maruhxn.rankademy.adapter.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+}
