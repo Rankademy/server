24f28c4ed231389774d84a9fc17899a6943c0f0f
maruhxn <maruhan1016@gmail.com>
2025-07-22 01:28:12 +0900

    chore: Add WebFlux

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,6 +61,9 @@ dependencies {
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 
+    // webflux
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     mockitoAgent("org.mockito:mockito-core:5.18.0") { isTransitive = false }
 }
 
