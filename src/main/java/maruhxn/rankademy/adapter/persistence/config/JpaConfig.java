07122093f11efe47a169320abcf5e5993beb4d6c
maruhxn <maruhan1016@gmail.com>
2025-08-06 14:55:35 +0900

    chore: JPAQueryFactoryBean

--- src/main/java/maruhxn/rankademy/adapter/persistence/config/JpaConfig.java
+++ src/main/java/maruhxn/rankademy/adapter/persistence/config/JpaConfig.java
@@ -1,6 +1,9 @@
 package maruhxn.rankademy.adapter.persistence.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,4 +13,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "maruhxn.rankademy.domain")
 @EnableJpaRepositories(basePackages = "maruhxn.rankademy.application")
 public class JpaConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
+    }
 }
