4e48c58d7a499eae156e669f516ffcd758268483
maruhxn <maruhan1016@gmail.com>
2025-08-10 21:33:32 +0900

    feat: RecruitmentPostApi 추가

---
+++ src/main/java/maruhxn/rankademy/adapter/webapi/RecruitmentPostApi.java
@@ -0,0 +1,60 @@
+package maruhxn.rankademy.adapter.webapi;
+
+import lombok.RequiredArgsConstructor;
+import maruhxn.rankademy.adapter.security.model.RankademyUser;
+import maruhxn.rankademy.application.group.provided.GroupReader;
+import maruhxn.rankademy.application.group.provided.RecruitPostWriter;
+import maruhxn.rankademy.application.group.provided.dto.RecruitmentPostDetailResponse;
+import maruhxn.rankademy.application.group.provided.dto.RecruitmentPostResponse;
+import maruhxn.rankademy.domain.group.dto.CreateRecruitmentPostRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/groups")
+@RequiredArgsConstructor
+public class RecruitmentPostApi {
+
+    private final GroupReader groupReader;
+    private final RecruitPostWriter recruitPostWriter;
+
+    @GetMapping("/posts")
+    public List<RecruitmentPostResponse> getRecruitmentPostList(
+            @RequestParam("page") int page
+    ) {
+        return groupReader.getRecruitmentPostList(page);
+    }
+
+    @GetMapping("/{groupId}/post")
+    public RecruitmentPostDetailResponse getRecruitmentPostDetails(
+            @AuthenticationPrincipal RankademyUser rankademyUser,
+            @PathVariable Long groupId
+    ) {
+        return groupReader.getRecruitmentPostDetail(rankademyUser.getId(), groupId);
+    }
+
+    @PostMapping("/{groupId}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("@groupLeaderChecker.isGroupLeader(#rankademyUser.userInfo(), #groupId)")
+    public void upsertRecruitmentPost(
+            @AuthenticationPrincipal RankademyUser rankademyUser,
+            @PathVariable Long groupId,
+            @RequestBody CreateRecruitmentPostRequest request
+    ) {
+        recruitPostWriter.upsertRecruitmentPost(groupId, request);
+    }
+
+    @PatchMapping("/{groupId}/posts/up")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("@groupLeaderChecker.isGroupLeader(#rankademyUser.userInfo(), #groupId)")
+    public void upRecruitmentPost(
+            @AuthenticationPrincipal RankademyUser rankademyUser,
+            @PathVariable Long groupId
+    ) {
+        recruitPostWriter.up(groupId);
+    }
+}
