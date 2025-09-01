247af371540853d5d9b4540af21db1527a97b2ae
maruhxn <maruhan1016@gmail.com>
2025-09-02 00:27:03 +0900

    feat: TeamRepository 메서드 추가(까먹음)

--- src/main/java/maruhxn/rankademy/application/team/required/TeamRepository.java
+++ src/main/java/maruhxn/rankademy/application/team/required/TeamRepository.java
@@ -1,7 +1,9 @@
 package maruhxn.rankademy.application.team.required;
 
 import maruhxn.rankademy.domain.team.Team;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -10,5 +12,8 @@ public interface TeamRepository extends Repository<Team, Long> {
     Team save(Team team);
 
     Optional<Team> findById(Long id);
+
+    @Query("select t from Team t join fetch t.teamMembers tm join fetch tm.user where t.id = :id")
+    Optional<Team> findByIdWithTeamMember(@Param("id") Long id);
 
 }
