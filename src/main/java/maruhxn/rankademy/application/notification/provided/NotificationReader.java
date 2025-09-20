feee509e9d5f0d65fb24d6ec978cd1dccf609c31
maruhxn <maruhan1016@gmail.com>
2025-09-20 13:04:40 +0900

    feat: NotificationReader 추가

--- src/main/java/maruhxn/rankademy/application/notification/provided/NotificationReader.java
+++ src/main/java/maruhxn/rankademy/application/notification/provided/NotificationReader.java
@@ -1,8 +1,11 @@
 package maruhxn.rankademy.application.notification.provided;
 
 import maruhxn.rankademy.application.notification.provided.dto.NotificationPageResponse;
+import maruhxn.rankademy.domain.notification.Notification;
 
 public interface NotificationReader {
+
+    Notification get(Long notificationId);
 
     NotificationPageResponse getNotifications(Long userId, int page);
 
