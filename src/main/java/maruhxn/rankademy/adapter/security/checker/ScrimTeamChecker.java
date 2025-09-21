0d565eba851225f921a46cc105b08bebfe56fd51
maruhxn <maruhan1016@gmail.com>
2025-09-21 20:43:49 +0900

    feat #16: ScrimTeamApi 추가

---
+++ src/main/java/maruhxn/rankademy/adapter/security/checker/ScrimTeamChecker.java
@@ -0,0 +1,20 @@
+package maruhxn.rankademy.adapter.security.checker;
+
+import lombok.RequiredArgsConstructor;
+import maruhxn.rankademy.adapter.security.model.UserInfo;
+import maruhxn.rankademy.application.scrim_team.provided.ScrimTeamReader;
+import maruhxn.rankademy.domain.scrim_team.ScrimTeam;
+import org.springframework.stereotype.Component;
+
+@Component("scrimTeamChecker")
+@RequiredArgsConstructor
+public class ScrimTeamChecker {
+
+    private final ScrimTeamReader scrimTeamReader;
+
+    public boolean isScrimTeamLeader(UserInfo user, Long scrimTeamId) {
+        ScrimTeam scrimTeam = scrimTeamReader.get(scrimTeamId);
+
+        return scrimTeam.isRepresentative(user.id());
+    }
+}
