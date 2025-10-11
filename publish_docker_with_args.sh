c88ef10e8fb8e3353e4b3356a8bdc70da45ec3c0
maruhxn <maruhxn@aswemake.com>
2025-10-11 17:39:46 +0900

    fix: 오타수정

--- publish_docker_with_args.sh
+++ publish_docker_with_args.sh
@@ -6,5 +6,5 @@ fi
 build_version=$(./extract_version.sh)
 version_code=$1
 
-docker build --platform linux/amd64 --build-arg versionCode="$build_version" -t ghcr.io/rankademy/rankademy-serve:"$version_code" .
-docker push ghcr.io/rankademy/rankademy-serve:"$version_code"
+docker build --platform linux/amd64 --build-arg versionCode="$build_version" -t ghcr.io/rankademy/rankademy-server:"$version_code" .
+docker push ghcr.io/rankademy/rankademy-server:"$version_code"
