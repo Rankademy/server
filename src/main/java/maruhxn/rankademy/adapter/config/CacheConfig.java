3f911d6dcf22cc4e4a52513833444c5f423090b6
maruhxn <maruhan1016@gmail.com>
2025-10-23 20:27:15 +0900

    feat: ddragon 최신 버전 조회 API 캐시 1일 TTL 적용

--- src/main/java/maruhxn/rankademy/adapter/config/CacheConfig.java
+++ src/main/java/maruhxn/rankademy/adapter/config/CacheConfig.java
@@ -1,9 +1,30 @@
 package maruhxn.rankademy.adapter.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+        List<CaffeineCache> caches = List.of(
+                new CaffeineCache("ddragonLatestVersion", Caffeine.newBuilder()
+                        .expireAfterWrite(1, TimeUnit.DAYS)
+                        .build())
+        );
+
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
 }
