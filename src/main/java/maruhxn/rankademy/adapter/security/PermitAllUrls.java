4e898422bca7d6015e8ba5b6ea0fa9b91793a7ae
maruhxn <maruhxn@aswemake.com>
2025-11-11 20:18:58 +0900

    fix: /api/v1/teams permitAll 제거

--- src/main/java/maruhxn/rankademy/adapter/security/PermitAllUrls.java
+++ src/main/java/maruhxn/rankademy/adapter/security/PermitAllUrls.java
@@ -29,7 +29,6 @@ public enum PermitAllUrls {
     SWAGGER_UI("/docs/swagger-ui.html", GET),
     SWAGGER_UI_RESOURCES("/docs/swagger-ui/**", GET),
     SWAGGER_API_DOCS("/docs/api-docs/**", GET),
-    TEAM_LIST("/api/v1/teams**", GET),
     SCRIM_TEAM_LIST("/api/v1/scrim-teams**", GET),
     SCRIM_TEAM_DETAILS("/api/v1/scrim-teams/{scrimTeamId}", GET),
     GROUP_COMPETITIONS_LIST("/api/v1/competitions/groups/{groupId}**", GET),
